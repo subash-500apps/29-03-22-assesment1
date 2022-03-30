5432f557a72646dce05c4ae0434a49518dd19393
subash-500apps <102018284+subash-500apps@users.noreply.github.com>
2022-03-30 16:07:47 +0530

    Add files via upload

---
+++ Qn2.vue
@@ -0,0 +1,66 @@
+<template>
+
+<div>
+
+<table>
+
+<thead>
+
+<tr>
+
+<th>Name</th>
+
+</tr>
+
+</thead>
+
+<tbody>
+
+<tr v-for="users in items" :key="users.id">
+
+<td>{{users.name}}</td>
+
+<td>{{users.index}}</td>
+
+</tr>
+
+</tbody>
+
+</table>
+
+</div>
+
+</template>
+
+
+<script>
+
+export default {
+
+name:"TABLEa23",
+
+data() {
+
+return {
+
+fields:['name','description'],
+
+items: [
+
+{"name":" Agile CRM" , "description" :" A company of excellence"},
+
+{"name":"mantra technologies", "description":"500apps" },
+
+{"name":"500apps Python", "description":"Backend built using python" },
+
+{"name":"500apps infinity", "description":"Built using vue JS" },
+
+]
+
+}
+
+}
+
+}
+
+</script>
